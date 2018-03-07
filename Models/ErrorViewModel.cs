c3d109ca2fcee42cf26941b63f80247c47ab6205
Tiago Bernardo <hsbtiago@gmail.com>
2018-03-06 21:29:58 -0400

    Projeto Criado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tambaqui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
