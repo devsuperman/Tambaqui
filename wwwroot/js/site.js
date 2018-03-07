875f5c25bba6d871b731753775496bae69c4ae83
Tiago Bernardo <hsbtiago@gmail.com>
2018-03-07 13:32:01 -0400

    Modelos de Select2 no script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,36 @@
-﻿// Write your JavaScript code.
-var x = 1;
+﻿
+$(document).ready(function(){
+
+    $('select').select2({
+        language: "pt-BR"
+    });
+
+
+
+
+    //Select2 Assíncrono, bom para listar munícipios!!!
+    var url_buscarMunicipio = '/municipios/listar';
+    $('.carregarMunicipios').select2({
+        placeholder: "Selecione",
+        language: "pt-BR",
+        minimumInputLength: 3,
+        ajax: {
+            url: url_buscarMunicipio,
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return {
+                    search: params.term
+                };
+            },
+            processResults: function (data) {
+                return {
+                    results: data
+                }
+            },
+            cache: true
+        }
+    });
+
+});
+
